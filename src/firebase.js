a67cd5f98d703feb7bdca5d40659f29fe56ac232
Ankit Bhati <60701684+ankit-bhati883@users.noreply.github.com>
2022-06-12 13:02:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import 'firebase/compat/firestore';
 
 import { collection, query, where, getDocs ,addDoc } from "firebase/firestore"; 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB-AZlvmQsQVWVLKU9zabyihVyj_cMxLfA",
-    authDomain: "nextgenschool-871e4.firebaseapp.com",
-    projectId: "nextgenschool-871e4",
-    storageBucket: "nextgenschool-871e4.appspot.com",
-    messagingSenderId: "739795588657",
-    appId: "1:739795588657:web:a58ec04a1a15eb87cfe710"
-  };
+//Api
 
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
